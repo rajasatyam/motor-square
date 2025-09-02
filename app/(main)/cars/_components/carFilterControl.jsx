e6c58fb8b3abc6553058accfe6f530655137b960
piyush <piyush02040@gmail.com>
2025-09-02 08:31:58 +0530

    car filter page ui done

--- app/(main)/cars/_components/carFilterControl.jsx
+++ app/(main)/cars/_components/carFilterControl.jsx
@@ -85,14 +85,25 @@ const CarFilterControl = ({
         
 
         </h4>
+        <div className='flex flex-wrap gap-2 max-h-60 overflow-y-auto pr-1'>
         {section?.options?.map((option)=>(
-          <Badge key={option.value}>
+          <Badge key={option.value} variant={
+            section.currentValue === option.value ?'default':'outline'
+          }
+           className={`cursor-pointer px-3 py-1 ${section.currentValue === option.value?'bg-blue-100 hover:bg-blue-200 text-blue-900 border-blue-200':'bg-white hobver;bg-gray-100 hover:bg-gray-100 text-gray-700 '}`}
+           onClick={()=>{
+            section.onChange(section.currentValue===option.value?"":option.value)
+           }}
+          
+          >
             {option.label}
             {section.currentValue === option.value && (
               <Check className="ml-1 h-3 w-3 inline" />
             )}
+            
           </Badge>
         ))}
+        </div>
       </div>
      ))}
     </div>
